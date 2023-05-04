0ee71ed2a831b13344c0ec8cc9899a4bb58a8259
Kenneth Schuetz <rane@mstsage.com>
2023-05-04 17:28:08 -0400

    - created varriables a, b, and c - created a variable for the sum of a, b, and c - created a variable for the product of a, b, and c printed it out using cout

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,5 +5,17 @@ int main()
     std::cout << "You are a Secret Agent for the Government infilitrating an Enemy Base...";
     std::cout << std::endl;
     std::cout << "You will need to enter the code in order to make it through....";
+
+    int a = 4;
+    int b = 10;
+    int c = 30;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product;
+
     return 0;
 }
