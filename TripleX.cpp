bf55633fffa43369de40a69b0bba10447070bbcf
Kenneth Schuetz <rane@mstsage.com>
2023-05-04 18:05:32 -0400

    - Added comments to explain code

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
+    // Print Game Welcome Message
     std::cout << "You are a Secret Agent for the Government infilitrating an Enemy Base...";
     std::cout << std::endl;
     std::cout << "You will need to enter the code in order to make it through....";
 
+    // Declare the 3 number code
     const int a = 4;
     const int b = 3;
     const int c = 2;
@@ -13,6 +15,7 @@ int main()
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    // Print Sum and Product to the Terminal
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product;
