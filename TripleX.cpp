e68aa6d8ce0ae1bca0990d6ba674b23137d91ed3
Kenneth Schuetz <rane@mstsage.com>
2023-05-04 15:44:02 -0400

    - Learned how to include a library - Added a string of text to put in mood for game. - used an std command to endline

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a Secret Agent for the Government infilitrating an Enemy Base...";
+    std::cout << std::endl;
+    std::cout << "You will need to enter the code in order to make it through....";
     return 0;
 }
