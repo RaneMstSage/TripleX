e266143e07ca80196dbcc8e4c70fda005b9ac184
Kenneth Schuetz <rane@mstsage.com>
2023-05-05 12:17:59 -0400

    - Updated Welcome Logo - Moved Introduction - Created a while loop and booleon for level complete.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void PrintIntroduction()
+void PrintWelcome()
 {
     // Print Game Welcome Message
     std::cout << " .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. \n";
@@ -13,37 +13,43 @@ void PrintIntroduction()
     std::cout << "| |  |__/  \\__|  | || | |_________|  | || |  |________|  | || |   `._____.'  | || |   `.____.'   | || ||_____||_____|| || | |_________|  | |\n";
     std::cout << "| |              | || |              | || |              | || |              | || |              | || |              | || |              | |\n";
     std::cout << "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |\n";
-    std::cout << " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' \n\n\n";                                                                                                                              
-    std::cout << "  TTTTTTTTTTTTTTTTTTTTTTT                      iiii                      lllllll                          XXXXXXX       XXXXXXX \n";
-    std::cout << "  T:::::::::::::::::::::T                     i::::i                     l:::::l                          X:::::X       X:::::X \n";
-    std::cout << "  T:::::::::::::::::::::T                      iiii                      l:::::l                          X:::::X       X:::::X \n";
-    std::cout << "  T:::::TT:::::::TT:::::T                                                l:::::l                          X::::::X     X::::::X \n";
-    std::cout << "  TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrr   iiiiiii ppppp   ppppppppp    l::::l     eeeeeeeeeeee         XXX:::::X   X:::::XXX \n";
-    std::cout << "          T:::::T        r::::rrr:::::::::r  i:::::i p::::ppp:::::::::p   l::::l   ee::::::::::::ee          X:::::X X:::::X    \n";
-    std::cout << "          T:::::T        r:::::::::::::::::r  i::::i p:::::::::::::::::p  l::::l  e::::::eeeee:::::ee         X:::::X:::::X     \n";
-    std::cout << "          T:::::T        rr::::::rrrrr::::::r i::::i pp::::::ppppp::::::p l::::l e::::::e     e:::::e          X:::::::::X       \n";
-    std::cout << "          T:::::T         r:::::r     r:::::r i::::i  p:::::p     p:::::p l::::l e:::::::eeeee::::::e          X:::::::::X       \n";
-    std::cout << "          T:::::T         r:::::r     rrrrrrr i::::i  p:::::p     p:::::p l::::l e:::::::::::::::::e          X:::::X:::::X     \n"; 
-    std::cout << "          T:::::T         r:::::r             i::::i  p:::::p     p:::::p l::::l e::::::eeeeeeeeeee          X:::::X X:::::X    \n";
-    std::cout << "          T:::::T         r:::::r             i::::i  p:::::p    p::::::p l::::l e:::::::e                XXX:::::X   X:::::XXX \n";
-    std::cout << "        TT:::::::TT       r:::::r            i::::::i p:::::ppppp:::::::pl::::::le::::::::e               X::::::X     X::::::X \n";
-    std::cout << "        T:::::::::T       r:::::r            i::::::i p::::::::::::::::p l::::::l e::::::::eeeeeeee       X:::::X       X:::::X \n";
-    std::cout << "        T:::::::::T       r:::::r            i::::::i p::::::::::::::pp  l::::::l  ee:::::::::::::e       X:::::X       X:::::X \n";
-    std::cout << "        TTTTTTTTTTT       rrrrrrr            iiiiiiii p::::::pppppppp    llllllll    eeeeeeeeeeeeee       XXXXXXX       XXXXXXX \n";
-    std::cout << "                                                      p:::::p                                                                   ";
-    std::cout << "                                              p:::::p                                                                   ";
-    std::cout << "                                             p:::::::p                                                                  ";
-    std::cout << "                                             p:::::::p                                                                  ";
-    std::cout << "                                             p:::::::p                                                                  ";
-    std::cout << "                                             ppppppppp                                                                  \n\n";                                                                                                                            
+    std::cout << " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'\n";                                                                                                                              
+    std::cout << std::endl;
+    std::cout << std::endl;                                                                                                                       
+    std::cout << "TTTTTTTTTTTTTTTTTTTTTTT                     iiii                     lllllll                          XXXXXXX       XXXXXXX\n";
+    std::cout << "T:::::::::::::::::::::T                    i::::i                    l:::::l                          X:::::X       X:::::X\n";
+    std::cout << "T:::::::::::::::::::::T                     iiii                     l:::::l                          X:::::X       X:::::X\n";
+    std::cout << "T:::::TT:::::::TT:::::T                                              l:::::l                          X::::::X     X::::::X\n";
+    std::cout << "TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrr  iiiiiiippppp   ppppppppp    l::::l     eeeeeeeeeeee         XXX:::::X   X:::::XXX\n";
+    std::cout << "        T:::::T        r:::::::::::::::::r i::::ip:::::::::::::::::p  l::::l  e::::::eeeee:::::ee         X:::::X:::::X\n";
+    std::cout << "        T:::::T        rr::::::rrrrr::::::ri::::ipp::::::ppppp::::::p l::::l e::::::e     e:::::e          X:::::::::X\n";
+    std::cout << "        T:::::T         r:::::r     r:::::ri::::i p:::::p     p:::::p l::::l e:::::::eeeee::::::e          X:::::::::X\n";
+    std::cout << "        T:::::T         r:::::r     rrrrrrri::::i p:::::p     p:::::p l::::l e:::::::::::::::::e          X:::::X:::::X\n";
+    std::cout << "        T:::::T         r:::::r            i::::i p:::::p    p::::::p l::::l e:::::::e                XXX:::::X   X:::::XXX\n";
+    std::cout << "      TT:::::::TT       r:::::r           i::::::ip:::::ppppp:::::::pl::::::le::::::::e               X::::::X     X::::::X\n";
+    std::cout << "      T:::::::::T       r:::::r           i::::::ip::::::::::::::::p l::::::l e::::::::eeeeeeee       X:::::X       X:::::X\n";
+    std::cout << "      T:::::::::T       r:::::r           i::::::ip::::::::::::::pp  l::::::l  ee:::::::::::::e       X:::::X       X:::::X\n";
+    std::cout << "      TTTTTTTTTTT       rrrrrrr           iiiiiiiip::::::pppppppp    llllllll    eeeeeeeeeeeeee       XXXXXXX       XXXXXXX\n";
+    std::cout << "                                                  p:::::p\n";
+    std::cout << "                                                  p:::::p\n";
+    std::cout << "                                                 p:::::::p\n";
+    std::cout << "                                                 p:::::::p\n";
+    std::cout << "                                                 p:::::::p\n";
+    std::cout << "                                                 ppppppppp\n";
+    std::cout << std::endl;                                                                                                                                                                                        
+}
+
+void PrintIntroduction()
+{
+    // Print Game Story
     std::cout << "You are a Secret Agent for the Government infilitrating an Enemy Base...\n";
     std::cout << "You will need to enter the correct code in order to make it through....\n\n";
 }
 
-void PlayGame()
-{
+bool PlayGame()
+{  
     PrintIntroduction();
-    
+
     const int CodeA = 4;
     const int CodeB = 3;
     const int CodeC = 2;
@@ -66,16 +72,26 @@ void PlayGame()
     // Check if the Player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\nYou win!!";
+        std::cout << "\nYou win!!\n\n";
+        return true;
     }
     else
     {
-        std::cout << "\nYou have failed this city!";
+        std::cout << "\nYou have failed this city!\n\n";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
+    PrintWelcome();
+
+    while (true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+    }
+    
     return 0;
 }
